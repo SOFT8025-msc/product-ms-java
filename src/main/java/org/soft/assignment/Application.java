94add2d453ab6d1ad94a80c63d739c6e5991aff4
joanna.wojcik <joanna.wojcik@hotmail.com>
2018-05-18 21:34:49 +0100

    Adding eureka deps

--- src/main/java/org/soft/assignment/Application.java
+++ src/main/java/org/soft/assignment/Application.java
@@ -1,8 +1,12 @@
 package org.soft.assignment;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 
