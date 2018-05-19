f6546c276c60f925bbdd887736aa87caafcef453
joanna.wojcik <joanna.wojcik@hotmail.com>
2018-05-19 16:55:01 +0100

    Eureka experiment1

--- src/main/java/org/soft/assignment/Application.java
+++ src/main/java/org/soft/assignment/Application.java
@@ -4,11 +4,20 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableAutoConfiguration
 @EnableEurekaClient
 @SpringBootApplication
+@RestController
+@Configuration
 public class Application {
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
